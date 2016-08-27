0d9a0e84c18bcf32019412af942b1360f505cfe0
bananatrooper52 <bananatrooper52@gmail.com>
2016-08-27 12:51:44 -1000

    Add scenes

---
+++ src/dev/rzebt52/scenes/_Game.java
@@ -0,0 +1,17 @@
+package dev.rzebt52.scenes;
+
+import java.awt.Graphics;
+
+public class _Game extends Scene {
+
+	public void tick() {
+		
+	}
+
+	public void render(Graphics g) {
+		
+		g.drawRect(0, 0, 300, 300);
+		
+	}
+	
+}
