26153892a8c781072bdd1af2d626d8d2cc1ea1a1
rze <carsonfreedman1@gmail.com>
2016-08-27 12:03:02 -1000

    Set up JFrame and basic run method.
    I was stuck on a bug for about 30 years but I fixed it!!!

---
+++ src/dev/rzebt52/Game.java
@@ -0,0 +1,125 @@
+package dev.rzebt52;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game implements Runnable {
+
+	public static final int WIDTH = 1366;
+	public static final int HEIGHT = 768;
+	public static final String NAME = "Game";
+	
+	public Thread thread;
+	public boolean running;
+	
+	private Canvas canvas;
+	private JFrame frame;
+
+	public Game() {
+		
+		frame = new JFrame(NAME);
+		canvas = new Canvas();
+		
+		frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+		frame.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(canvas, BorderLayout.CENTER);
+		frame.pack();
+		
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void run() {
+
+		int maxTps = 120;
+		int maxFps = 1000000;
+		double timePerTick = 1000000000 / maxTps;
+		double timePerFrame = 1000000000 / maxFps;
+		double deltaTicks = 0;
+		double deltaFrames = 0;
+		long now;
+		long lastTime = System.nanoTime();
+		long timer = 0;
+		int ticks = 0;
+		int frames = 0;
+
+		while (running) {
+			now = System.nanoTime();
+			deltaTicks += (now - lastTime) / timePerTick;
+			deltaFrames += (now - lastTime) / timePerFrame;
+			timer += now - lastTime;
+			lastTime = now;
+			
+			if (deltaTicks >= 1) {
+				
+				tick();
+				ticks++;
+				deltaTicks--;
+				
+			}
+			
+			if (deltaFrames >= 1) {
+				
+				render();
+				frames++;
+				deltaFrames--;
+				
+			}
+			
+			if (timer >= 1000000000) {
+				
+				System.out.println("TPS: " + ticks + " FPS: " + frames);
+				
+				ticks = 0;
+				frames = 0;
+				timer = 0;
+				
+			}
+			
+		}
+
+	}
+	
+	public synchronized void start() {
+		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+		
+	}
+	
+	public synchronized void stop() {
+		
+		running = false;
+		
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void tick() {
+
+	}
+	
+	public void render() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Game().start();
+	}
+
+}
